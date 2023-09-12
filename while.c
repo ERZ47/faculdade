5980ae6067fcc3d9520230d18509ef5d5d1bd70d
ERZ47 <143460348+ERZ47@users.noreply.github.com>
2023-09-11 20:24:21 -0400

    Create while.c

---
+++ while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    int op;
+    float n1,n2,operacao;
+    //entrada de dados
+    printf("\n\ninforme o numero 1:  ");
+    scanf("%f",&n1 );
+    printf("\n\ninfrome o numero 2:  ");
+    scanf("%f",&n2);
+    
+    op=0;
+    while (op !=5){
+        printf("bo !");   
+        printf("novo op:");
+        scanf("%d",&op);
+    }
+    
+    return 0;
+}
